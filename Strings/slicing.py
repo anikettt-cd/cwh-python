9f87e2031a82baa6f5b00a5d492986c960915367
anikettt-cd <sainianiket751@gmail.com>
2026-03-08 19:58:44 +0530

    strings

--- Strings/slicing.py
+++ Strings/slicing.py
@@ -3,3 +3,5 @@ name = "aniket0123456789"
 # print(name[2:-1])
 
 print(name[0:10:3])
+print(len(name[:4]))
+print(len(name[0:]))
---
+++ Strings/string_methods.py
@@ -0,0 +1,21 @@
+# a = "hello world "
+# s = len(a)
+# print(s)
+# print(a.upper())
+# print(a.lower())
+# print(a.capitalize())
+
+# text = "\nhello world "
+# print(text.strip())
+# print(text.lstrip())
+# print(text.rstrip())
+
+text = "python is fun"
+print(text.find("is"))
+print(text.replace( "fun", "awesome"))
+
+text = "apples, bananas, pineapples , manga"
+
+print(text.split(","))
+
+print(",".join(['apples', 'bananas', 'pineapples' , 'manga']))
