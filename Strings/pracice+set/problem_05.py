1ec4b761614299d0b20d5bf667160d95466729d9
anikettt-cd <sainianiket751@gmail.com>
2026-03-09 22:03:45 +0530

    string problems

---
+++ Strings/pracice+set/problem_05.py
@@ -0,0 +1,15 @@
+sentence = "Coding in python is fun"
+
+new = sentence.replace("fun", "awesome")
+print(new)
+
+sentence = "Coding in python is fun"
+
+ind = sentence.index("python")
+print(ind)
+
+a = sentence.upper()
+print(a)
+
+
+
---
+++ Strings/pracice+set/problem_06.py
@@ -0,0 +1,23 @@
+a = " hey my name is aniket, what is your name?"
+
+sum = 0
+vowels = ["a", "e", "i", "o", "u"]
+
+for char in a:
+    if(char in vowels):
+        sum+=1
+
+print(f" there are {sum} vowels in the string")
+
+print(a.count("a"))
+print(a.count("e"))
+print(a.count("i"))
+print(a.count("o"))
+print(a.count("u"))
+   
+    
+
+
+
+
+ 
