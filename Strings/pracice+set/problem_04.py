34089882be8cffa90e29b8cadff650cb892cb527
anikettt-cd <sainianiket751@gmail.com>
2026-03-09 21:51:19 +0530

    problem

--- Strings/pracice+set/problem_04.py
+++ Strings/pracice+set/problem_04.py
@@ -1,3 +1,7 @@
 name = input("Enter the name:")
 age = int(input("enter the age:"))
-print(f" My name is {name} and i am {age} years old")
+print(" My name is {} and i am {} years old.".format(name,age))
+
+name = input("Enter the name:")
+age = int(input("enter the age:"))
+print(f" My name is {name} and i am {age} years old.")
