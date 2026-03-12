a9a75a6ccdedbdc961956b5d411d4ac29aae42ad
anikettt-cd <sainianiket751@gmail.com>
2026-03-12 09:55:04 +0530

    update

---
+++ function and modules/lamda_function.py
@@ -0,0 +1,18 @@
+square = lambda x: x*x
+
+'''
+as good as writing
+ def squar(x):
+ return x*x
+'''
+
+sum = lambda x, y : x + y
+
+'''
+as good as writing
+def sum(x, y):
+    return x + y
+'''
+
+print(sum(34,34))
+print(square(5))
