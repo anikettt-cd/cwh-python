f9ab718d7305d71d3d9e46d7aa2fbe41d512de2b
anikettt-cd <sainianiket751@gmail.com>
2026-03-12 09:51:07 +0530

    function

---
+++ function and modules/function.py
@@ -0,0 +1,12 @@
+def average (a , b, c):
+    d = (a + b + c)/3
+    return(d)
+    
+o1 = average(4, 5, 4) 
+o2 = average(3, 3, 5)
+print(o1)
+print(o2)   
+
+
+
+    
---
+++ function and modules/function_arguments.py
@@ -0,0 +1,13 @@
+def add(a, b, ):
+    x = a + b 
+    return x
+
+c = add(2,3)
+print(c)
+
+def add(a, b, d = 0):
+    x = a + b + d
+    return x
+
+c = add(2,3,5)
+print(c)
