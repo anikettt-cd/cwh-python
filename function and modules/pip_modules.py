db3f1cc1da6a13f9c230a6c23a6d6508022d7cd6
anikettt-cd <sainianiket751@gmail.com>
2026-03-13 21:08:37 +0530

    modules

---
+++ function and modules/pip_modules.py
@@ -0,0 +1,17 @@
+
+
+# their are tw types of modules in python 
+# 1) built in modules
+# 2) external modules 
+
+
+import math
+import os
+import my_module
+import requests
+
+
+print(math.sqrt(4))
+my_module.hello()
+r = requests.get("https://www.google.com")
+print(r.text)
